744d70957698bc7457e222fca9d12556c76cfc20
MUTIA KALINDA CHRISTIAN <ckchristiankalinda5@gmail.com>
2026-05-25 17:23:09 +0300

    Update file paths for bulk inserts in SQL script

--- scripts/bronze/procedure_load_bronze.sql
+++ scripts/bronze/procedure_load_bronze.sql
@@ -76,7 +76,7 @@ BEGIN
         PRINT 'Inserting data into: bronze.crm_cust_info';
 
         BULK INSERT bronze.crm_cust_info
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        FROM 'datasets/source_crm/cust_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -103,7 +103,7 @@ BEGIN
         PRINT 'Inserting data into: bronze.crm_prd_info';
 
         BULK INSERT bronze.crm_prd_info
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        FROM 'datasets/source_crm/prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -128,7 +128,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_sales_details;
 
         BULK INSERT bronze.crm_sales_details
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        FROM 'datasets/source_crm/sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -159,7 +159,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_loc_a101;
 
         BULK INSERT bronze.erp_loc_a101
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+        FROM 'datasets/source_crm/LOC_A101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -184,7 +184,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_cust_az12;
 
         BULK INSERT bronze.erp_cust_az12
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+        FROM 'datasets/source_crm/CUST_AZ12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -209,7 +209,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'D:\My Lessons\SQL\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+        FROM 'datasets/source_crm/PX_CAT_G1V2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
